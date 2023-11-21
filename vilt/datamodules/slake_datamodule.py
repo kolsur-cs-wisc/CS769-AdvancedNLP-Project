4a761563e713296eb197312c561de4a77542b64e
Aditya Kolsur <kolsur@wisc.edu>
2023-11-21 14:12:15 -0600

    SLAKE Data Module

--- vilt/datamodules/slake_datamodule.py
+++ vilt/datamodules/slake_datamodule.py
@@ -1,3 +1,15 @@
 from vilt.datasets import SLAKEDataset
 from .datamodule_base import BaseDataModule
 from collections import defaultdict
+
+class SLAKEDataModule(BaseDataModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def dataset_cls(self):
+        return SLAKEDataset
+
+    @property
+    def dataset_name(self):
+        return "slake"
