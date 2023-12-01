f15cde73cde471c2ae707b1e943342d7ba909c0b
Aditya Kolsur <kolsur@wisc.edu>
2023-11-30 20:51:05 -0600

    Finetune Base ViLT on VQA RAD

--- finetune_slake.py
+++ finetune_slake.py
@@ -1,6 +1,6 @@
 import json
 import torch
-from PIL import Image
+import os
 from copy import deepcopy
 from transformers import ViltConfig, ViltProcessor, ViltForQuestionAnswering
 from torch.utils.data import DataLoader
@@ -40,36 +40,39 @@ def collate_fn(batch):
 
   return batch
 
-f = open('root/train.json')
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
+
+f = open('root_slake/train.json')
 train_json = json.load(f)
 
-f = open('root/test.json')
+f = open('root_slake/test.json')
 test_json = json.load(f)
 
-f = open('root/validate.json')
+f = open('root_slake/validate.json')
 validate_json = json.load(f)
 
 label_to_ans, ans_to_label = create_ans2label(train_json, validate_json, test_json)
 
-config = ViltConfig.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-mlm")
+processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
 train_dataset = SLAKE_Dataset(data = train_json, processor = processor, ans2label = ans_to_label, label2ans = label_to_ans)
 test_dataset = SLAKE_Dataset(data = test_json, processor = processor, ans2label = ans_to_label, label2ans = label_to_ans)
 validate_dataset = SLAKE_Dataset(data = validate_json, processor = processor, ans2label = ans_to_label, label2ans = label_to_ans)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 
 model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-mlm", id2label=label_to_ans, label2id=ans_to_label)
+model.classifier[3] = torch.nn.Linear(in_features=1536 , out_features=36, bias=True)
 model.to(device)
 
-train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn, batch_size=100, shuffle=True)
-validate_dataloader = DataLoader(validate_dataset, collate_fn=collate_fn, batch_size=len(validate_dataset), shuffle=True)
+train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn, batch_size=24, shuffle=True)
+validate_dataloader = DataLoader(validate_dataset, collate_fn=collate_fn, batch_size=8, shuffle=True)
+test_dataloader = DataLoader(test_dataset, collate_fn=collate_fn, batch_size=8, shuffle=True)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-3)
 
 model.train()
-for epoch in range(3):  # loop over the dataset multiple times
+for epoch in range(5):  # loop over the dataset multiple times
     print(f"Epoch: {epoch}")
 
     for batch in tqdm(train_dataloader):
@@ -81,7 +84,9 @@ for epoch in range(3):  # loop over the dataset multiple times
       print("Loss:", loss.item())
       loss.backward()
       optimizer.step()
-        
+    
+    correct = 0
+
     for batch in tqdm(validate_dataloader):
         batch = {k:v.to(device) for k,v in batch.items()}
 
@@ -91,5 +96,22 @@ for epoch in range(3):  # loop over the dataset multiple times
         predicted_classes = torch.sigmoid(logits)
         probs, classes = torch.topk(predicted_classes, 1)
 
-        true_labels = batch['labels'].nonzero(as_tuple=True)
-        print(true_labels)
+        true_labels = torch.tensor(batch['labels'].nonzero(as_tuple=True)[1])
+
+        correct += (classes[1] == true_labels).float().sum()
+    print("Validate Accuracy: ", correct/len(validate_dataset))
+
+correct = 0
+for batch in tqdm(test_dataloader):
+    batch = {k:v.to(device) for k,v in batch.items()}
+
+    outputs = model(**batch)
+    logits = outputs.logits
+
+    predicted_classes = torch.sigmoid(logits)
+    probs, classes = torch.topk(predicted_classes, 1)
+
+    true_labels = torch.tensor(batch['labels'].nonzero(as_tuple=True)[1])
+
+    correct += (classes[1] == true_labels).float().sum()
+print("Test Accuracy: ", correct/len(test_dataset))
